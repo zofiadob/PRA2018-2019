fa2aadce4cf13631665cfa1cc9109980c9842484
Ikarani <ikarani24@op.pl>
2018-10-08 13:14:31 +0200

    rename package

--- src/main/java/pracownia/introduction/Main.java
+++ src/main/java/pracownia/introduction/Main.java
@@ -1,8 +1,13 @@
 package pracownia.introduction;
 
+import org.apache.log4j.Logger;
+
 public class Main {
+
+    static Logger log = Logger.getLogger("asd");
 
     public static void main(String [ ] args) {
         System.out.println("Hello World");
+        log.info("Message");
     }
 }
